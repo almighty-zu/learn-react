51277ca404d9cae99312f617bd9b871bc5f4e41d
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-02-25 18:43:19 +0100

    Add REDUX to the project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import styles from './App.scss';
 import PropTypes from 'prop-types';
-//import List from '../List/List.js';
-//import {pageContents, listData} from '../../data/dataStore';
+import List from '../List/ListContainer.js';
+//import {listData} from '../../data/dataStore';
 
 class App extends React.Component {
   static propTypes = {
     title: PropTypes.node,
-    subile: PropTypes.node,
+    subtitle: PropTypes.node,
+    lists: PropTypes.array,
   }
+
   render() {
-    const {title, subtitle} = this.props;
+    const {title, subtitle, lists} = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        {/*
-        <List {...listData} />
-        */}
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
       </main>
     );
   }
