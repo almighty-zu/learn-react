be850230e69b78762cb24dfec13b64418d82ae04
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-02-27 16:18:38 +0100

    Add changes in Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 //import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
+import { settings } from '../../data/dataStore';
 //import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
@@ -11,6 +12,10 @@ class Column extends React.Component {
     title: PropTypes.node.isRequired,
     icon: PropTypes.string,
     cards: PropTypes.array,
+  }
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   }
   render() {
     const {title, icon, cards} = this.props;
